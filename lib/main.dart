8c9743028bfa9cb02e7c8e53edf8aadf107fabf6
Lans12345 <olanalans12345@gmail.com>
2024-03-26 10:12:39 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pharma_invenqurec/firebase_options.dart';
 import 'package:pharma_invenqurec/screens/landing_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'pharma-2857b',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
